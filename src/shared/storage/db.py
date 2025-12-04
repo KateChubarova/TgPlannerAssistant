85b268333167a0f96a777d8a3e840c3a840d2779
katsiarynach <katsiaryna.chubarava@innowise.com>
2025-12-04 19:02:46 +0100

    16 docker init

--- src/shared/storage/db.py
+++ src/shared/storage/db.py
@@ -6,14 +6,14 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, future=True)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://k.chubarava:12345@db:5432/postgres",
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-)
